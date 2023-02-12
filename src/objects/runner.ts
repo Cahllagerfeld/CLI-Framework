d34dc075abec05c0a8657cf1bb20123192f3a40d
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-02-12 19:37:00 +0000

    feat: parse args & params

--- src/objects/runner.ts
+++ src/objects/runner.ts
@@ -1,1 +1,13 @@
-export class Runner {}
+import { parseArgs } from "./tools/parse-params.js";
+
+export class Runner {
+	run: (this: Runner, args: string | string[]) => Promise<void>;
+	constructor() {
+		this.run = Run;
+	}
+}
+
+export async function Run(this: Runner, args: string | string[]) {
+	const parsedArgs = await parseArgs(args);
+	console.log(parsedArgs);
+}
---
+++ src/objects/tools/parse-params.ts
@@ -0,0 +1,19 @@
+export async function parseArgs(commandArray: string | string[]) {
+	const { default: yargsParser } = await import("yargs-parser");
+
+	if (typeof commandArray === "string") {
+		commandArray = commandArray.split(" ");
+	}
+
+	commandArray = commandArray as string[];
+
+	if (process.argv === commandArray) {
+		commandArray = commandArray.slice(2);
+	}
+
+	const parsedArgs = yargsParser(commandArray);
+	const args = parsedArgs._.slice();
+	delete parsedArgs._;
+
+	return { ...parsedArgs, args };
+}
