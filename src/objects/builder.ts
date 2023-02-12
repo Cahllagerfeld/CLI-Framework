6d9161a048ddf0b422221aa3363727e9909c84d9
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2023-02-12 16:14:45 +0000

    feat: small additions

--- src/objects/builder.ts
+++ src/objects/builder.ts
@@ -1,1 +1,33 @@
-export class Builder {}
+import { Runner } from "./runner.js";
+
+class Builder {
+	private brand: string;
+	private nativeDirectory: string;
+	private pluginDirectory: string;
+
+	constructor(brand: string) {
+		this.brand = brand;
+		this.nativeDirectory = "";
+		this.pluginDirectory = "";
+	}
+
+	public src(directory: string): Builder {
+		this.nativeDirectory = directory;
+		return this;
+	}
+
+	public plugins(pluginRegex: string) {
+		this.pluginDirectory = pluginRegex;
+		return this;
+	}
+
+	public build(): Runner {
+		const runner = new Runner();
+
+		return runner;
+	}
+}
+
+export function build(brand: string): Builder {
+	return new Builder(brand);
+}
